a1707ea6bddc75a8b6b76ab61e431bbb9cb3e220
AIT BAHADDOU Ayoub <ayoubaitbahaddou@gmail.com>
2022-11-07 20:48:15 +0100

    Solve Factorise N+M form Codeforces

---
+++ 11-7-22/FactoriseNPlusM.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+//https://codeforces.com/problemset/problem/1740/A
+int main() {
+    int t; cin>>t;
+    while (t--) {
+        int n; cin>>n;
+        cout<<n<<endl;
+    }
+}
