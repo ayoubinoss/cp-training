f0ad87dcfbe7db6df2dd51b7f7198752c0684d06
AIT BAHADDOU Ayoub <ayoubaitbahaddou@gmail.com>
2022-11-08 21:34:16 +0100

    Solve Immobile knight from codeforces

---
+++ 11-8-22/ImmobileKnight.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+//https://codeforces.com/contest/1739/problem/A
+int main() {
+    int t; cin>>t;
+    while (t--) {
+        int n,m; cin>>n>>m;
+        cout<<n/2+1<<" "<<m/2+1<<endl;
+    }
+}
